6eab7d5c7921abe0cb9be4db1de9ff997fce3714
Francisco Revilla <frevilla@phoenixcontact.com>
2018-01-13 12:45:01 +0100

    First commit for the anmeldung form.

---
+++ anmeldung/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+SERIE_GERMANY = (('GPS-100', 'GPS-100'), ('GPS-250', 'GPS-250'), ('GPS-500', 'GPS-500'), ('GPS-1000', 'GPS-1000'), ('GPS-1200', 'GPS-1200'))
+CATEGORY_GERMANY = (('Herren A', 'Herren A'), ('Herren B', 'Herren B'), ('Damen', 'Damen'), ('Mixed', 'Mixed'), ('Senioren', 'Senioren'), ('Junioren', 'Junioren'))
+PLAYER = (('A', 'A'), ('B', 'B'))
+
+
+class Tournament(models.Model):
+    category = models.CharField(choices=SERIE_GERMANY, max_length=20)
+    city = models.CharField(max_length=20)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return " ".join([str(self.date) + str(self.category) + str(self.city)])
+
+
+class Registration(models.Model):
+    # tournament_name = models.ForeignKey(Tournament)
+    # category = models.CharField(choices=CATEGORY_GERMANY, max_length=32)
+    # player = models.CharField(choices=PLAYER, max_length=32)
+    forename = models.CharField(max_length=24, verbose_name='First Name')
+    surname = models.CharField(max_length=24, verbose_name='Last Name')
+    email = models.EmailField(verbose_name='Email')
+    phone = models.CharField(max_length=64, verbose_name='Phone')
+    city = models.CharField(max_length=32, verbose_name='City')
+    club = models.CharField(max_length=32, verbose_name='Club')
+    birthplace = models.CharField(max_length=32, verbose_name='Birthday')
+    #birthdate = forms.CharField(initial=arrival_initial_date, validators=[arrival_date_validator])
+    ranking_points = models.PositiveIntegerField(max_length=32, verbose_name='Ranking Points')
+
+
---
+++ anmeldung/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django import forms
+from anmeldung.models import Registration
+
+
+class RegistrationForm(forms.ModelForm):
+
+    class Meta:
+        model = Registration
+        fields = '__all__'
+        widgets = {
+            'player': forms.TextInput(attrs={'placeholder': 'Hast du bereits ein GPS - Turnier gespielt? Such dich!'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Mail-Adresse'}),
+            'surname': forms.TextInput(attrs={'placeholder': 'Familienname'}),
+            'forename': forms.TextInput(attrs={'placeholder': 'Vorname'}),
+            'phone': forms.TextInput(attrs={'placeholder': 'Telefonnummer'}),
+        }
+
+
+def index(request):
+    print("Hola Landing")
+    return render(request, 'landing.html')
+
+
+def anmeldung(request):
+    if request.method == 'POST':
+        registration_form = RegistrationForm(request.POST)
+        if registration_form.is_valid():
+            print("Form is valid :)")
+            return render(request, 'turnierliste.html')
+        else:
+            print("Form is invalid :(")
+            return render(request, 'anmeldung.html', {'form': registration_form})
+    else:
+        return render(request, 'anmeldung.html', {'form': RegistrationForm()})
+
+
+def turnierliste(request):
+    print("Hola Turnierliste")
+    return render(request, 'turnierliste.html')
+
+
