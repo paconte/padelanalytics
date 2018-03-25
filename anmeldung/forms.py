0cc699dfaed1569cce827db59b91bbcc2d52187e
Francisco Revilla <paconte@gmail.com>
2018-03-25 16:45:33 +0200

    Remake tournament signup.

--- anmeldung/forms.py
+++ anmeldung/forms.py
@@ -6,23 +6,10 @@ from anmeldung.validators import policy_read_validator, DATE_FORMAT, convert_dat
 
 
 class RegistrationForm(forms.ModelForm):
-    category = forms.ChoiceField(choices=CATEGORY_GERMANY, widget=forms.RadioSelect)
-
     class Meta:
         model = Registration
         fields = '__all__'
-        widgets = {
-            'player': forms.TextInput(attrs={'placeholder': 'Hast du bereits ein GPS - Turnier gespielt? Such dich!'}),
-            'email': forms.EmailInput(attrs={'placeholder': 'Mail-Adresse'}),
-            'surname': forms.TextInput(attrs={'placeholder': 'Familienname'}),
-            'forename': forms.TextInput(attrs={'placeholder': 'Vorname'}),
-            'phone': forms.TextInput(attrs={'placeholder': 'Telefonnummer'}),
-            'city': forms.TextInput(attrs={'placeholder': 'Wohnort'}),
-            'club': forms.TextInput(attrs={'placeholder': 'Verein'}),
-            'birthplace': forms.TextInput(attrs={'placeholder': 'Geburtsort'}),
-            'ranking_points': forms.TextInput(attrs={'placeholder': 'Trag deine Punkte ein'}),
-            'birthdate': forms.TextInput(attrs={'placeholder': 'Geburtsdatum'}),
-        }
+        exclude = ['creation_date']
 
 
 class FullRegistrationForm(forms.Form):
