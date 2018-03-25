cb85a358d583ecb3906342308b36bfb7e697880b
Fran González <fragonser@gmail.com>
2018-03-25 17:45:44 +0200

    Adapting the form for a new player

--- anmeldung/forms.py
+++ anmeldung/forms.py
@@ -185,15 +185,17 @@ class FullRegistrationForm(forms.Form):
 class NewPlayerForm(forms.ModelForm):
     class Meta:
         model = Player
-        exclude = ['creation_date', 'surname2', 'country', 'ranking_points', 'photo']
+        exclude = ['creation_date', 'ranking_points', 'photo']
         widgets = {
             'email': forms.EmailInput(attrs={'placeholder': 'Mail-Adresse'}),
             'surname': forms.TextInput(attrs={'placeholder': 'Familienname'}),
+            'surname2': forms.TextInput(attrs={'placeholder': 'Familienname 2'}),
             'forename': forms.TextInput(attrs={'placeholder': 'Vorname'}),
             'phone': forms.TextInput(attrs={'placeholder': 'Telefonnummer'}),
             'city': forms.TextInput(attrs={'placeholder': 'Wohnort'}),
             'club': forms.Select(choices=Club.objects.all(), attrs={'placeholder': 'Verein'}),
             'birthplace': forms.TextInput(attrs={'placeholder': 'Geburtsort'}),
             'birthdate': forms.TextInput(attrs={'placeholder': 'Geburtsdatum'}),
-            'policy_read': forms.CheckboxInput(attrs={'placeholder': 'Muerte con pinchos'})
+            'country': forms.Select(attrs={'placeholder': 'Land'}),
+            'policy_read': forms.CheckboxInput(attrs={'placeholder': 'Accept'})
         }
