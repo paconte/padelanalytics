a831a399bf7f9bd9a7da7b09bbec34d6863379c4
Francisco Revilla <paconte@gmail.com>
2018-05-10 15:33:12 +0200

    Use tournament.models at anmeldung.models class and rename some anmeldung.models classes and fields.

--- anmeldung/models.py
+++ anmeldung/models.py
@@ -1,9 +1,13 @@
 from decimal import Decimal
+
+from django.core.validators import MaxValueValidator
+from django.core.validators import MinValueValidator
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
-from anmeldung.validators import policy_read_validator
 from django_countries.fields import CountryField
 
+from anmeldung.validators import policy_read_validator
+from tournaments.models import Person
+from tournaments.models import Tournament
 
 SERIE_GERMANY = (('GPS-100', 'GPS-100'), ('GPS-250', 'GPS-250'), ('GPS-500', 'GPS-500'), ('GPS-1000', 'GPS-1000'),
                  ('GPS-1200', 'GPS-1200'), ('GPS-WOMEN', 'GPS-WOMEN'))
@@ -14,7 +18,7 @@ PLAYER = (('A', 'A'), ('B', 'B'))
 
 def player_directory_path(instance, filename):
     return 'player_media/' + normalize(
-        '{0}-{1}-{2}-{3}'.format(instance.surname, instance.forename, instance.email, filename))
+        '{0}-{1}-{2}-{3}'.format(instance.last_name, instance.first_name, instance.email, filename))
 
 
 def club_directory_path(instance, filename):
@@ -38,70 +42,63 @@ class Club(models.Model):
         return self.name
 
 
-class Tournament(models.Model):
-    creation_date = models.DateTimeField(auto_now_add=True, editable=False)
+class PadelTournament(Tournament):
     club = models.ForeignKey(Club, on_delete=models.DO_NOTHING)
-    serie = models.CharField(choices=SERIE_GERMANY, max_length=20, default='GPS-500')
-    category = models.CharField(choices=CATEGORY_GERMANY, max_length=20, default='Herren A')
-    city = models.CharField(max_length=20)
-    date = models.DateField()
+    padel_serie = models.CharField(choices=SERIE_GERMANY, max_length=20, default='GPS-500')
+    padel_category = models.CharField(choices=CATEGORY_GERMANY, max_length=20, default='Herren A')
     signup = models.BooleanField(default=False)
     finished = models.BooleanField(default=False)
 
     @property
     def serie_url(self):
-        if self.serie == 'GPS-100':
+        if self.padel_serie == 'GPS-100':
             return 'images/kategorien/gps100.jpg'
-        elif self.serie == 'GPS-250':
+        elif self.padel_serie == 'GPS-250':
             return 'images/kategorien/gps250.jpg'
-        elif self.serie == 'GPS-500':
+        elif self.padel_serie == 'GPS-500':
             return 'images/kategorien/gps500.jpg'
-        elif self.serie == 'GPS-1000':
+        elif self.padel_serie == 'GPS-1000':
             return 'images/kategorien/gps1000.jpg'
-        elif self.serie == 'GPS-1200':
+        elif self.padel_serie == 'GPS-1200':
             return 'images/kategorien/gps1200.jpg'
-        elif self.serie == 'GPS-WOMEN':
+        elif self.padel_serie == 'GPS-WOMEN':
             return 'images/kategorien/w-gps.jpg'
         else:
             raise TypeError("The serie is not supported.")
 
     def __str__(self):
-        return "  /  ".join([str(self.date), str(self.serie), str(self.city), str(self.category)])
+        return "  /  ".join([str(self.date), str(self.padel_serie), str(self.city), str(self.padel_category)])
 
     def turnierliste_key(self):
-        return " ".join([str(self.serie), str(self.city), str(self.date)])
+        return " ".join([str(self.padel_serie), str(self.city), str(self.date)])
 
 
-class Player(models.Model):
-    creation_date = models.DateTimeField(auto_now_add=True, editable=False)
-    forename = models.CharField(max_length=24, verbose_name='First Name')
-    surname = models.CharField(max_length=24, verbose_name='Last Name')
-    surname2 = models.CharField(max_length=24, blank=True)
+class PadelPerson(Person):
+    last_name2 = models.CharField(max_length=24, blank=True)
     email = models.EmailField(verbose_name='Email')
     phone = models.CharField(max_length=64, verbose_name='Phone')
     country = CountryField()
     city = models.CharField(max_length=32, verbose_name='City')
     club = models.ForeignKey(Club, on_delete=models.DO_NOTHING)
     birthplace = models.CharField(max_length=32, verbose_name='Birth place')
-    birthdate = models.DateTimeField(verbose_name='Birthday')
     ranking_points = models.DecimalField(
         max_digits=6, decimal_places=1, validators=[MinValueValidator(Decimal('0.0'))], default=Decimal('0.0'))
     photo = models.ImageField(upload_to=player_directory_path, default='Cool-Male-Avatars-06.png')
     policy_read = models.BooleanField(default=False, validators=[policy_read_validator])
 
     def __str__(self):
-        return " ".join([str(self.forename), str(self.surname)])
+        return " ".join([str(self.last_name), str(self.last_name)])
 
     def abbr(self):
-        return " ".join([self.forename[0] + '.', self.surname])
+        return " ".join([self.first_name[0] + '.', self.last_name])
 
 
 class Registration(models.Model):
     creation_date = models.DateTimeField(auto_now_add=True, editable=False)
     tournament = models.ForeignKey(Tournament, on_delete=models.DO_NOTHING)
     policy_read = models.BooleanField(default=False, validators=[policy_read_validator])
-    player_a = models.ForeignKey(Player, related_name="player_a", on_delete=models.DO_NOTHING)
-    player_b = models.ForeignKey(Player, related_name="player_b", on_delete=models.DO_NOTHING)
+    player_a = models.ForeignKey(PadelPerson, related_name="player_a", on_delete=models.DO_NOTHING)
+    player_b = models.ForeignKey(PadelPerson, related_name="player_b", on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return " - ".join([str(self.player_a), str(self.player_b)])
