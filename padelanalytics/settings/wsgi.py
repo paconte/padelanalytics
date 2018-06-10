e3afab4b0dd965d213f251ef08fd950aebc0c9aa
Francisco Revilla <paconte@gmail.com>
2018-06-10 20:41:21 +0200

    Create settings for development and production.

--- padelanalytics/settings/wsgi.py
+++ padelanalytics/settings/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "padelanalytics.settings")
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "padelanalytics.settings.prod")
+# load secret variables. File is not to find at the repository!
+SECRETS_FILE = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'settings/secrets.py')
+exec(open(SECRETS_FILE).read())
 application = get_wsgi_application()
