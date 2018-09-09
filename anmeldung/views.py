6118fd40174c01b1e6840fb779d99c98d5784adf
Fran González <fragonser@gmail.com>
2018-09-09 18:01:55 +0200

    Success New Page Page

--- anmeldung/views.py
+++ anmeldung/views.py
@@ -39,15 +39,16 @@ def index(request):
 
 
 def test_view(request):
-    return render(request, 'tournament_signup_success.html',
-                 {'email_a': 'paco@gmail.com',
-                  'email_b': 'fran@gmail.com',
-                  'from_email': 'info@padelanalytics.com'
-                  })
+    #return render(request, 'tournament_signup_success.html',
+    #            {'email_a': 'paco@gmail.com',
+    #             'email_b': 'fran@gmail.com',
+    #             'from_email': 'info@padelanalytics.com'
+    #             })
     #return render(request, 'tournament_signup_activation.html')
     #return render(request, 'activation_failed.html')
     #return render(request, '404.html')
     #return render(request, '500.html')
+    return render(request, 'new_player_success.html')
 
 
 def tournament_signup(request, id=None):
